45fa100057c18ca084f66c494e6ed35a2e1f8efe
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-01-25 14:17:54 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+
+// const category = 'toys';
+//
+// console.log(`https://someurl.com/${category}/5`);
+//
+// const user = 'Ivan';
+//
+// alert(`Hi, ${user}`);
+
+
+
+// let result = confirm("Are you here?");
+// console.log(result);
+//
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answer = [];
+//
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как ваша фамилия?', '');
+// answer[2] = prompt('Сколько вам лет?', '');
+//
+//
+// console.log(typeof(answer));
+// console.log(typeof(null));
